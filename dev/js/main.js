c30741c3fe6a4c9d2ef8a616d46d803660a0b5b2
virzen <wiktor.czajkowski@gmail.com>
2015-12-11 21:36:28 +0100

    Add displaying defined in model feature
    Model contains object representing places on the map. In the initMap()
    marker is created for each one of them.

---
+++ dev/js/main.js
@@ -0,0 +1,74 @@
+(function () {
+	var model = {
+		startCoords: {
+			lat: 53.1269068,
+			lng: 18.0282105
+		},
+		places: [
+			// template
+			// {
+			// 	name: '',
+			// 	position: {
+			// 		lat: ,
+			// 		lng:
+			// 	}
+			// }
+			{
+				name: 'Opera Nova',
+				position: {
+					lat: 53.124556,
+					lng: 17.997279
+				}
+			},
+			{
+				name: 'Focus Park',
+				position: {
+					lat: 53.1247133,
+					lng: 18.0180284
+				}
+			}
+		]
+	};
+
+
+	var ViewModel = function () {
+		var self = this;
+
+		self.map = {};
+		self.places = model.places;
+		self.markers = [];
+
+		window.initMap = function () {
+			self.map = new google.maps.Map(document.querySelector('.map'), {
+				center: model.startCoords,
+				zoom: 12
+			});
+
+			for (place of self.places) {
+				var marker = new google.maps.Marker({
+					position: place.position,
+					title: place.name,
+					map: self.map
+				});
+			}
+			// var marker = new google.maps.Marker({
+			// 	position: defCoords,
+			// 	title: 'Hello World!',
+			// 	animation: google.maps.Animation.DROP,
+			// 	map: map,
+			// });
+			//
+			// var infoWindow = new google.maps.InfoWindow({
+			// 	content: '<h2>Some text</h2><p>Even more text</p>',
+			// });
+			//
+			//
+			// marker.addListener('click', function () {
+			// 	infoWindow.open(map, marker);
+			// }, false);
+		};
+	};
+
+
+	ko.applyBindings(new ViewModel());
+})();
